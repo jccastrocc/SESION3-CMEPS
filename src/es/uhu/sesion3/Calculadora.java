e3a0c5325f8ef7bb008d85b618766c9254765abe
jccastrocc <jccastroc.develop@gmail.com>
2024-10-07 16:50:58 +0200

    Tercer commit: El codigo refactorizado y ver que las pruebas pasan

--- src/es/uhu/sesion3/Calculadora.java
+++ src/es/uhu/sesion3/Calculadora.java
@@ -2,10 +2,12 @@ package es.uhu.sesion3;
 
 public class Calculadora {
 
-	public static Integer suma(int i, int j) {
+	
+	
+	public static Integer suma(int a, int b) {
 
 		
-		return 5;
+		return a+b;
 	}
 
 }
