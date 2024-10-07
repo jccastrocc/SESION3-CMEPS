0644520909e5aa81e30ccba9e34bbf520e8e1751
jccastrocc <jccastroc.develop@gmail.com>
2024-10-07 16:42:57 +0200

    Primer commit

---
+++ src/es/uhu/sesion3/test/CalculadoresTest.java
@@ -0,0 +1,43 @@
+package es.uhu.sesion3.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoresTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	void testSuma() {
+		
+		//int float
+		
+		
+		assertEquals(5,Calculadora.suma(3,2));
+	}
+
+}
